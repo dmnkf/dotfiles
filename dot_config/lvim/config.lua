a490be8f57d9d98dab3b1b4b2bc4df3145ff30a7
Dominik Filliger <filligerdominik@gmail.com>
2024-08-05 00:32:29 +0200

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -245,7 +245,13 @@ copilot.setup {
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
 
-
+-- TERMINAL 
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
+}
 
 -- Enable core plugins
 lvim.builtin.project.active = true
