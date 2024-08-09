ef571d6975c45e7fe279e8f423e032ecc6a4b0d0
Dominik Filliger <filligerdominik@gmail.com>
2024-08-09 15:03:17 +0200

    Add .config/lvim/after/queries/markdown/highlights.scm Add .config/lvim/after/queries/markdown/textobjects.scm Add .config/lvim/after/queries/markdown_inline/highlights.scm Add .config/lvim/after/queries/norg/injections.scm Add .config/lvim/after/queries/python/highlights.scm Add .config/lvim/after/queries/python/textobjects.scm Add .config/lvim/after/queries/r/highlights.scm Add .config/lvim/after/queries/r/textobjects.scm Add .config/lvim/after/queries/rust/injections.scm Update .config/lvim/config.lua Add .config/lvim/ftplugin/bash.lua Add .config/lvim/ftplugin/haskell.lua Add .config/lvim/ftplugin/markdown.lua Add .config/lvim/ftplugin/neotest-output.lua Add .config/lvim/ftplugin/python.lua Add .config/lvim/ftplugin/quarto.lua Add .config/lvim/ftplugin/r.lua Add .config/lvim/ftplugin/sh.lua Add .config/lvim/lazy-lock.json Add .config/lvim/queries/markdown/textobjects.scm Add .config/lvim/queries/r/textobjects.scm Add .config/lvim/snips/package.json Add .config/lvim/snips/snippets/css.json Add .config/lvim/snips/snippets/global.json Add .config/lvim/snips/snippets/json.json Add .config/lvim/snips/snippets/markdown.json Add .config/lvim/snips/snippets/quarto.json Add .config/lvim/snips/snippets/tex.json

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -18,6 +18,7 @@ lvim.plugins = {
     event = "InsertEnter",
     lazy = false
   },
+
   {
     "zbirenbaum/copilot-cmp",
     after = {"copilot.lua"},
@@ -25,6 +26,7 @@ lvim.plugins = {
       require("copilot_cmp").setup()
     end
   },
+
   {
       "vhyrro/luarocks.nvim",
       priority = 1001, -- this plugin needs to run before anything else
@@ -32,6 +34,7 @@ lvim.plugins = {
           rocks = { "magick" },
       },
   },
+
   {
     -- for lsp features in code cells / embedded code
     'jmbuhr/otter.nvim',
@@ -44,41 +47,105 @@ lvim.plugins = {
     },
     opts = {},
   },
-  {
-      "3rd/image.nvim",
-      dependencies = { "luarocks.nvim" },
-      opts = {
-        backend = "kitty",
-         integrations = {
+
+  { -- show images in nvim!
+    '3rd/image.nvim',
+    enabled = true,
+    dev = false,
+    ft = { 'markdown', 'quarto', 'vimwiki' },
+    config = function()
+      -- Requirements
+      -- https://github.com/3rd/image.nvim?tab=readme-ov-file#requirements
+      -- check for dependencies with `:checkhealth kickstart`
+      -- needs:
+      -- sudo apt install imagemagick
+      -- sudo apt install libmagickwand-dev
+      -- sudo apt install liblua5.1-0-dev
+      -- sudo apt install lua5.1
+      -- sudo apt installl luajit
+
+      local image = require 'image'
+      image.setup {
+        backend = 'kitty',
+        integrations = {
           markdown = {
             enabled = true,
-            clear_in_insert_mod = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-          },
-          neorg = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            filetypes = { "norg" },
-          },
-          html = {
-            enabled = false,
-          },
-          css = {
-            enabled = false,
+            only_render_image_at_cursor = true,
+            filetypes = { 'markdown', 'vimwiki', 'quarto' },
           },
         },
-        max_width = 100,
-        max_height = 12,
-        max_height_window_percentage = math.huge,
-        max_width_window_percentage = math.huge,
+        editor_only_render_when_focused = false,
         window_overlap_clear_enabled = true,
-        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+        -- window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', 'scrollview' },
+        tmux_show_only_in_active_window = true,
+        window_overlap_clear_ft_ignore = { 'cmp_menu', 'cmp_docs', 'scrollview', 'scrollview_sign' },
+        max_width = nil,
+        max_height = nil,
+        max_width_window_percentage = nil,
+        max_height_window_percentage = 30,
+        kitty_method = 'normal',
       }
+
+      local function clear_all_images()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local images = image.get_images { buffer = bufnr }
+        for _, img in ipairs(images) do
+          img:clear()
+        end
+      end
+
+      local function get_image_at_cursor(buf)
+        local images = image.get_images { buffer = buf }
+        local row = vim.api.nvim_win_get_cursor(0)[1] - 1
+        for _, img in ipairs(images) do
+          if img.geometry ~= nil and img.geometry.y == row then
+            local og_max_height = img.global_state.options.max_height_window_percentage
+            img.global_state.options.max_height_window_percentage = nil
+            return img, og_max_height
+          end
+        end
+        return nil
+      end
+
+      local create_preview_window = function(img, og_max_height)
+        local buf = vim.api.nvim_create_buf(false, true)
+        local win_width = vim.api.nvim_get_option_value('columns', {})
+        local win_height = vim.api.nvim_get_option_value('lines', {})
+        local win = vim.api.nvim_open_win(buf, true, {
+          relative = 'editor',
+          style = 'minimal',
+          width = win_width,
+          height = win_height,
+          row = 0,
+          col = 0,
+          zindex = 1000,
+        })
+        vim.keymap.set('n', 'q', function()
+          vim.api.nvim_win_close(win, true)
+          img.global_state.options.max_height_window_percentage = og_max_height
+        end, { buffer = buf })
+        return { buf = buf, win = win }
+      end
+
+      local handle_zoom = function(bufnr)
+        local img, og_max_height = get_image_at_cursor(bufnr)
+        if img == nil then
+          return
+        end
+
+        local preview = create_preview_window(img, og_max_height)
+        image.hijack_buffer(img.path, preview.win, preview.buf)
+      end
+
+      vim.keymap.set('n', '<leader>io', function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        handle_zoom(bufnr)
+      end, { buffer = true, desc = 'image [o]pen' })
+
+      vim.keymap.set('n', '<leader>ic', clear_all_images, { desc = 'image [c]lear' })
+    end,
   },
+
   {
     'quarto-dev/quarto-nvim',
     ft = { 'quarto' },
@@ -99,6 +166,7 @@ lvim.plugins = {
       'jmbuhr/otter.nvim',
     },
   },
+
   {
     'GCBallesteros/jupytext.nvim',
     opts = {
@@ -111,6 +179,7 @@ lvim.plugins = {
       },
     },
   },
+
   {
     'HakonHarnes/img-clip.nvim',
     event = 'BufEnter',
@@ -141,20 +210,25 @@ lvim.plugins = {
       vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
     end,
   },
+
   {
     'mrjones2014/smart-splits.nvim',
     build = './kitty/install-kittens.bash'
   },
+
   {
     'gsuuon/tshjkl.nvim',
     config = true
   },
+
   {
     "chrisgrieser/nvim-spider", lazy = true
   },
+
   {
     "stevearc/dressing.nvim"
   },
+
   {
     "coffebar/transfer.nvim",
     lazy = true,
@@ -296,19 +370,22 @@ lvim.plugins = {
     end,
     keys = {
       { '<leader>mi', ':MoltenInit<cr>', desc = '[m]olten [i]nit' },
-      {
+
+  {
         '<leader>mv',
         ':<C-u>MoltenEvaluateVisual<cr>',
         mode = 'v',
         desc = 'molten eval visual',
       },
-      { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
+
+  { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
     },
   },
 }
 
 vim.list_extend(lvim.builtin.cmp.sources, {
   { name = "otter" },
+
   { name = "copilot"},
 })
 
