a85add212e28402064ac562a0fa7422172e4dabc
Dominik Filliger <filligerdominik@gmail.com>
2024-08-09 15:09:38 +0200

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -268,116 +268,116 @@ lvim.plugins = {
     },
   },
 
-  { -- send code from python/r/qmd documets to a terminal or REPL
-    -- like ipython, R, bash
-    -- may be replaced with conjure in the future.
-    'jpalardy/vim-slime',
-    dev = false,
-    init = function()
-      vim.b['quarto_is_python_chunk'] = false
-      Quarto_is_in_python_chunk = function()
-        require('otter.tools.functions').is_otter_language_context 'python'
-      end
+  -- { -- send code from python/r/qmd documets to a terminal or REPL
+  --   -- like ipython, R, bash
+  --   -- may be replaced with conjure in the future.
+  --   'jpalardy/vim-slime',
+  --   dev = false,
+  --   init = function()
+  --     vim.b['quarto_is_python_chunk'] = false
+  --     Quarto_is_in_python_chunk = function()
+  --       require('otter.tools.functions').is_otter_language_context 'python'
+  --     end
 
-      vim.cmd [[
-      let g:slime_dispatch_ipython_pause = 100
-      function SlimeOverride_EscapeText_quarto(text)
-      call v:lua.Quarto_is_in_python_chunk()
-      if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk && !(exists('b:quarto_is_r_mode') && b:quarto_is_r_mode)
-      return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
-      else
-      if exists('b:quarto_is_r_mode') && b:quarto_is_r_mode && b:quarto_is_python_chunk
-      return [a:text, "\n"]
-      else
-      return [a:text]
-      end
-      end
-      endfunction
-      ]]
+  --     vim.cmd [[
+  --     let g:slime_dispatch_ipython_pause = 100
+  --     function SlimeOverride_EscapeText_quarto(text)
+  --     call v:lua.Quarto_is_in_python_chunk()
+  --     if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk && !(exists('b:quarto_is_r_mode') && b:quarto_is_r_mode)
+  --     return ["%cpaste -q\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+  --     else
+  --     if exists('b:quarto_is_r_mode') && b:quarto_is_r_mode && b:quarto_is_python_chunk
+  --     return [a:text, "\n"]
+  --     else
+  --     return [a:text]
+  --     end
+  --     end
+  --     endfunction
+  --     ]]
 
-      vim.g.slime_target = 'neovim'
-      vim.g.slime_no_mappings = true
-      vim.g.slime_python_ipython = 1
-    end,
-    config = function()
-      vim.g.slime_input_pid = false
-      vim.g.slime_suggest_default = true
-      vim.g.slime_menu_config = false
-      vim.g.slime_neovim_ignore_unlisted = true
+  --     vim.g.slime_target = 'neovim'
+  --     vim.g.slime_no_mappings = true
+  --     vim.g.slime_python_ipython = 1
+  --   end,
+  --   config = function()
+  --     vim.g.slime_input_pid = false
+  --     vim.g.slime_suggest_default = true
+  --     vim.g.slime_menu_config = false
+  --     vim.g.slime_neovim_ignore_unlisted = true
 
-      local function mark_terminal()
-        local job_id = vim.b.terminal_job_id
-        vim.print('job_id: ' .. job_id)
-      end
+  --     local function mark_terminal()
+  --       local job_id = vim.b.terminal_job_id
+  --       vim.print('job_id: ' .. job_id)
+  --     end
 
-      local function set_terminal()
-        vim.fn.call('slime#config', {})
-      end
-      vim.keymap.set('n', '<leader>cm', mark_terminal, { desc = '[m]ark terminal' })
-      vim.keymap.set('n', '<leader>cs', set_terminal, { desc = '[s]et terminal' })
-    end,
-  },
+  --     local function set_terminal()
+  --       vim.fn.call('slime#config', {})
+  --     end
+  --     vim.keymap.set('n', '<leader>cm', mark_terminal, { desc = '[m]ark terminal' })
+  --     vim.keymap.set('n', '<leader>cs', set_terminal, { desc = '[s]et terminal' })
+  --   end,
+  -- },
 
-  { -- paste an image from the clipboard or drag-and-drop
-    'HakonHarnes/img-clip.nvim',
-    event = 'BufEnter',
-    ft = { 'markdown', 'quarto', 'latex' },
-    opts = {
-      default = {
-        dir_path = 'img',
-      },
-      filetypes = {
-        markdown = {
-          url_encode_path = true,
-          template = '![$CURSOR]($FILE_PATH)',
-          drag_and_drop = {
-            download_images = false,
-          },
-        },
-        quarto = {
-          url_encode_path = true,
-          template = '![$CURSOR]($FILE_PATH)',
-          drag_and_drop = {
-            download_images = false,
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require('img-clip').setup(opts)
-      vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
-    end,
-  },
+  -- { -- paste an image from the clipboard or drag-and-drop
+  --   'HakonHarnes/img-clip.nvim',
+  --   event = 'BufEnter',
+  --   ft = { 'markdown', 'quarto', 'latex' },
+  --   opts = {
+  --     default = {
+  --       dir_path = 'img',
+  --     },
+  --     filetypes = {
+  --       markdown = {
+  --         url_encode_path = true,
+  --         template = '![$CURSOR]($FILE_PATH)',
+  --         drag_and_drop = {
+  --           download_images = false,
+  --         },
+  --       },
+  --       quarto = {
+  --         url_encode_path = true,
+  --         template = '![$CURSOR]($FILE_PATH)',
+  --         drag_and_drop = {
+  --           download_images = false,
+  --         },
+  --       },
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     require('img-clip').setup(opts)
+  --     vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
+  --   end,
+  -- },
 
-  { -- preview equations
-    'jbyuki/nabla.nvim',
-    keys = {
-      { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
-    },
-  },
+  -- { -- preview equations
+  --   'jbyuki/nabla.nvim',
+  --   keys = {
+  --     { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [m]ath equations' },
+  --   },
+  -- },
 
-  {
-    'benlubas/molten-nvim',
-    enabled = false,
-    build = ':UpdateRemotePlugins',
-    init = function()
-      vim.g.molten_image_provider = 'image.nvim'
-      vim.g.molten_output_win_max_height = 20
-      vim.g.molten_auto_open_output = false
-    end,
-    keys = {
-      { '<leader>mi', ':MoltenInit<cr>', desc = '[m]olten [i]nit' },
+  -- {
+  --   'benlubas/molten-nvim',
+  --   enabled = false,
+  --   build = ':UpdateRemotePlugins',
+  --   init = function()
+  --     vim.g.molten_image_provider = 'image.nvim'
+  --     vim.g.molten_output_win_max_height = 20
+  --     vim.g.molten_auto_open_output = false
+  --   end,
+  --   keys = {
+  --     { '<leader>mi', ':MoltenInit<cr>', desc = '[m]olten [i]nit' },
 
-  {
-        '<leader>mv',
-        ':<C-u>MoltenEvaluateVisual<cr>',
-        mode = 'v',
-        desc = 'molten eval visual',
-      },
+  -- {
+  --       '<leader>mv',
+  --       ':<C-u>MoltenEvaluateVisual<cr>',
+  --       mode = 'v',
+  --       desc = 'molten eval visual',
+  --     },
 
-  { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
-    },
-  },
+  -- { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
+  --   },
+  -- },
 }
 
 vim.list_extend(lvim.builtin.cmp.sources, {
@@ -405,14 +405,6 @@ copilot.setup {
 
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
-
--- TERMINAL 
-lvim.builtin.which_key.mappings["t"] = {
-  name = "+Terminal",
-  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
-  v = { "<cmd>2ToggleTerm size=100 direction=vertical<cr>", "Split vertical" },
-  h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
-}
 
 -- Enable core plugins
 lvim.builtin.project.active = true
