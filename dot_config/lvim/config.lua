2eff93362a6e0e14d74a2359cbd062e4b443f2a2
Dominik Filliger <filligerdominik@gmail.com>
2024-08-14 13:23:06 +0200

    Update .config/lvim/config.lua Add .config/lvim/.chezmoiignore

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -641,23 +641,7 @@ else
 end
 
 local function send_cell()
-  if vim.b['quarto_is_r_mode'] == nil then
-    vim.fn['slime#send_cell']()
-    return
-  end
-  if vim.b['quarto_is_r_mode'] == true then
-    vim.g.slime_python_ipython = 0
-    local is_python = require('otter.tools.functions').is_otter_language_context 'python'
-    if is_python and not vim.b['reticulate_running'] then
-      vim.fn['slime#send']('reticulate::repl_python()' .. '\r')
-      vim.b['reticulate_running'] = true
-    end
-    if not is_python and vim.b['reticulate_running'] then
-      vim.fn['slime#send']('exit' .. '\r')
-      vim.b['reticulate_running'] = false
-    end
-    vim.fn['slime#send_cell']()
-  end
+  vim.fn['slime#send_cell']()
 end
 
 
